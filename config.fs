3b4ecb413bbf4602bff8bbcf696ffed8661fa879
klozz <klozz707@gmail.com>
2017-12-29 00:56:04 -0500

    msm8956-common: add some qcom aids
    Signed-off-by: klozz <klozz707@gmail.com>

--- config.fs
+++ config.fs
@@ -1,28 +1,40 @@
-[system/bin/pm-service]
+[AID_QCOM_DIAG]
+value:2950
+ 
+[AID_RFS]
+value:2951
+
+[AID_RFS_SHARED]
+value:2952
+
+[AID_QTI_DIAG]
+value:2901
+
+[system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
+[system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
+[system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/ims_rtp_daemon]
+[system/vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[system/bin/cnss-daemon]
+[system/vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
